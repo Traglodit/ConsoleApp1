021a3872f1283240d853f2b1bed94006d1b15309
peche <peche@LAPTOP-J20VB09N>
2020-11-06 12:59:02 +0300

    v2

--- Program.cs
+++ Program.cs
@@ -2,33 +2,54 @@
 
 namespace PR1_1
 {
-    struct Person
+    /// <summary>
+    /// Класс для человека
+    /// описание
+    /// 
+    /// </summary>
+    class Person
     {
+        public enum Genders : int
+        {
+            Male,
+            Female,
+            custom
+        };
         public string firstName;
         public string lastName;
         public int age;
-
-        public Person(string _firstName, string _lastName, int _age)
+        public Genders gender;
+        public Person(string _firstName, string _lastName, int _age, Genders _gender)
         {
             firstName = _firstName;
             lastName = _lastName;
             age = _age;
+            gender = _gender;
         }
         public override string ToString()
         {
-            return firstName + " " + lastName + ", age " + age;
+            return firstName + " " + lastName + " (" + gender + "), age " + age;
         }
-
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Имя</param>
+        public void Dislay(string name)
+        {
+            Console.WriteLine(name);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Person p = new Person("Kats", "Vasily", 18);
-            Console.WriteLine(p.ToString());
-            Console.ReadLine();
+            Person p = new Person("Василий", "Кац", 19, Person.Genders.Male);
+            p.Dislay("Васян");
+            Person p1 = new Person("Vasily", "Kats", 19, Person.Genders.custom);
+            Console.WriteLine(p);
+            Console.WriteLine(p1);
+            Console.ReadKey();
         }
     }
 }
